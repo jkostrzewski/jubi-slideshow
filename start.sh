2c7de93144b065a5efe96269c3b9f99435c0d4bc
jkostrzewski <jakubkost@gmail.com>
2016-03-19 13:45:33 +0100

    added php server to start

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
 export DISPLAY=:0
-nohup chromium-browser --kiosk --start-maximized --incognito ~/jubi-slideshow/index.html&
+cd ~/jubi-slideshow/index.htm && php -S 0.0.0.0:8000
+nohup chromium-browser --kiosk --start-maximized --incognito http://localhost:8000&
