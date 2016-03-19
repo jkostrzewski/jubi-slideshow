c485dbe5e0ee7f0c00ed2e13889f957a1e3e09b1
jkostrzewski <jakubkost@gmail.com>
2016-03-19 13:48:04 +0100

    fix

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 export DISPLAY=:0
-cd ~/jubi-slideshow/index.htm && php -S 0.0.0.0:8000
+cd ~/jubi-slideshow/index.htm && nohup php -S 0.0.0.0:8000&
 nohup chromium-browser --kiosk --start-maximized --incognito http://localhost:8000&
